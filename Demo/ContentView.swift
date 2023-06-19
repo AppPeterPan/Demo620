5e65495745ef8517b6b0f3702d75e26ba8c3bd66
peter <apppeterpan@gmail.com>
2023-06-20 01:21:21 +0800

    update2

--- Demo/ContentView.swift
+++ Demo/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, wendy!")
+            Text("Hello, mike!")
         }
         .padding()
     }
